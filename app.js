5b789144c670eaa17c2d6408eedcbb995355ec3b
Brunogpr <guilhermebrunopr@gmail.com>
2022-11-22 00:34:18 -0300

    Definindo Body e tratando CORS

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const morgan = require('morgan') 
+const bodyParser = require('body-parser')
 const app = express()
 
 const rotaProdutos = require('./routes/produtos')
 const rotaPedidos = require('./routes/pedidos')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false})) //aceita apenas dados simples
+app.use(bodyParser.json()) //aceita apenas json no body
+
+app.use((req, res, next) => {
+    res.header('Acces-Control-Allow-Origin', '*')
+    res.header('Acces-Control-Allow-Header', 
+                'Origin, X-Requrested-With, Content-Type, Accept, Authorization')
+    
+    if (req.method === 'OPTIONS') {
+        res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).send({})
+    }
+
+    next()
+    
+})
 
 app.use('/produtos', rotaProdutos)
 app.use('/pedidos', rotaPedidos)
